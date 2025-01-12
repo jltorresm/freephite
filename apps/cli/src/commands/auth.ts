e7a7f47d62678ed955ca47b67883f9b314db920c
Jose Torres <jose.torres.tm@gmail.com>
2025-01-12 15:47:32 +0000

    Remove duplicate auth command.

--- apps/cli/src/commands/auth.ts
+++ apps/cli/src/commands/auth.ts
@@ -6,15 +6,14 @@ const args = {
   token: {
     type: 'string',
     alias: 't',
-    describe: 'Auth token. Get it from: https://app.graphite.dev/activate',
+    describe: 'Github Auth token from: https://github.com/settings/tokens',
     demandOption: false,
   },
 } as const;
 type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
 
 export const command = 'auth';
-export const description =
-  'Add your auth token to enable Graphite CLI to create and update your PRs on GitHub.';
+export const description = 'Add your Github auth token.';
 export const builder = args;
 export const canonical = 'auth';
 
@@ -22,13 +21,18 @@ export const handler = async (argv: argsT): Promise<void> => {
   return graphiteWithoutRepo(argv, canonical, async (context) => {
     if (argv.token) {
       context.userConfig.update((data) => (data.authToken = argv.token));
+      context.userConfig.update((data) => (data.fpAuthToken = argv.token));
       context.splog.info(
         chalk.green(`🔐 Saved auth token to "${context.userConfig.path}"`)
       );
       return;
     }
-    context.splog.info(
-      context.userConfig.getAuthToken() ?? 'No auth token set.'
-    );
+
+    const existing = context.userConfig.getFPAuthToken();
+    if (existing) {
+      context.splog.info(`Existing auth token: ${existing}`);
+    } else {
+      context.splog.error('No auth token set.');
+    }
   });
 };
